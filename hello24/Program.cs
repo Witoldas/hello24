dfdcbe8374028ec9e31a5597372387293e5106ad
Witoldas <70074120+Witoldas@users.noreply.github.com>
2024-02-28 14:45:01 +0100

    Add project files.

---
+++ hello24/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World24!");
+            Console.WriteLine("Witej Witku.");
+        }
+    }
+}
